5cd9e94688ac4c23049c8b0d27152d0963215a0d
qxornet <arhangel1998i@mail.ru>
2019-03-22 11:34:43 +0400

    UPD

--- web/index.php
+++ web/index.php
@@ -10,16 +10,41 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+
+	return 'Hello, World!';
+	
+});
+
+$app->post('/bot', function() use($app) {
+	$data = json_decode(file_get_contents('php://input'));
+	
+	if(!$data)
+		return 'NULL';
+	
+	if($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type != 'confirmation' )
+		return 'NULL';
+	
+	
+	switch($data->type)
+	{
+		
+		case 'confirmation':
+		return getenv('VK_SECRET_TOKEN');
+		break;
+		
+		case 'messages_new':
+		
+		
+		
+		break;
+		
+	}
+	
+	return 'NULL';
+	
 });
 
 $app->run();
